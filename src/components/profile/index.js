0ab6f563b4d0dbf619f2342647fa3342f67a9958
“Maikel <design@maikelverbeek.nl>
2023-10-19 16:01:28 +0200

    refactor: back to basis:
    Deleted posts because of integration with Medium.
    Deleted unused files.
    Restructured the app.
    First start with useSWR.

---
+++ src/components/profile/index.js
@@ -0,0 +1,16 @@
+import useSWR from 'swr'
+import fetcher from '../../utils/swr-helper' 
+ 
+export default function Profile() {
+  const { data, error } = useSWR('/api/profile-data', fetcher)
+ 
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+ 
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.bio}</p>
+    </div>
+  )
+}
---
+++ src/utils/swr-helper.js
@@ -0,0 +1,3 @@
+import useSWR from 'swr'
+
+export default function fetcher(...args) { fetch(...args).then(res => res.json())};
