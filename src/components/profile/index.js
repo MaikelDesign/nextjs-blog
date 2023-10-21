c87f9296a19235bd0ebc40c2db8a2ab086032775
“Maikel <design@maikelverbeek.nl>
2023-10-21 16:03:48 +0200

    test: git hook eslint test

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,18 +1,19 @@
 import useSWR from 'swr'
 import fetcher from '../../utils/swr-helper'
 
-const name = "string";
+const name = 'string'
 
 export default function Profile() {
-  const { data, error } = useSWR('/api/profile-data', fetcher)
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+	const { data, error } = useSWR('/api/profile-data', fetcher)
 
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.bio}</p>
-    </div>
-  )
+	if (error) return <div>Failed to load</div>
+	if (!data) return <div>Loading...</div>
+
+	return (
+		<div>
+			<h1>{data.name}</h1>
+			<p>{data.bio}</p>
+		</div>
+	)
 }
