1b1e95f0c5626abcc691c49e5206ff616f93f57d
“Maikel <design@maikelverbeek.nl>
2024-01-03 19:56:49 +0100

    fix: sitemap generation

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ export async function getStaticProps() {
   }
 }
 
+
 export default function Index({ allPostsData }) {
   return (
     <DefaultTemplate home>
--- src/utils/generate-sitemap.js
+++ src/utils/generate-sitemap.js
@@ -3,26 +3,46 @@ const path = require('path')
 const glob = require('glob')
 
 function generateSiteMap() {
-  const pages = glob.sync('pages/**/*.js', { ignore: ['pages/_*.js', 'pages/api/**/*.js'] })
+  const staticPages = glob.sync('./src/pages/**/*.js', { ignore: ['./src/pages/_*.js', './src/pages/api/**/*.js', './src/pages/posts/*.js'] })
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages
-    .map(page => {
-      const path = page
-        .replace('pages', '')
+  const postsDirectory = path.join(process.cwd(), 'src/posts')
+  const fileNames = fs.readdirSync(postsDirectory)
+  const postIds = fileNames.map((fileName) => {
+    return {
+      id: fileName.replace(/\.md$/, ''),
+    }
+  })
+
+  const urls = [
+    ...staticPages.map(page => {
+      const path = page 
+        .replace('./src/pages', '')
         .replace('.js', '')
         .replace('.tsx', '')
         .replace('/index', '')
 
-      return `
+      return { url: `https://www.maikelverbeek.com${path === '' ? '/' : path}`, changefreq: 'daily', priority: 0.7 }
+    }),
+    ...postIds.map(id => {
+      return {
+        url: `https://www.maikelverbeek.com/posts/${id.id}`,
+        changefreq: 'weekly',
+        priority: 0.5
+      }
+    })
+  ]
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${urls
+    .map(urlObj => `
             <url>
-                <loc>${`https://www.maikelverbeek.com${path === '' ? '/' : path}`}</loc>
-                <changefreq>daily</changefreq>
-                <priority>0.7</priority>
+                <loc>${urlObj.url}</loc>
+                <changefreq>${urlObj.changefreq}</changefreq>
+                <priority>${urlObj.priority}</priority>
             </url>
         `
-    })
+    )
     .join('')}
     </urlset>
     `
